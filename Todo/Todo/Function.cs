f6d0c3e254e9195d15cb6e5d52c8c4e6ba2fd43a
Claude Agnes <rlawnstjddl12@gmail.com>
2023-03-15 14:46:47 +0900

    first

---
+++ Todo/Todo/Function.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo
+{
+    class Function
+    {
+        static string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\CS_TodoList");
+        static string txtpath = path + @"\todo.txt";
+        public static void WelCome()
+        {
+            Console.WriteLine("Welcome to Todo List!");
+            Console.WriteLine();
+            Console.WriteLine("add [your todo] : input your todo");
+            Console.WriteLine("rm [remove todo's id] : delete your Todos");
+            Console.WriteLine("ls : show your Todos.");
+            Console.WriteLine("clear : Clear Todos.");
+        }
+
+        public static void AddCommand(string todo)
+        {
+            StreamWriter writer;
+            writer = File.AppendText(txtpath);
+            writer.Write(todo + "\n");
+            Console.WriteLine($"{todo}was added");
+            writer.Close();
+        }
+
+        public static void RemoveCommand(int num)
+        {
+            var file = new List<string>(File.ReadAllLines(txtpath));
+            if (num - 1 >= file.Count)
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                string rmtodo = file[num - 1];
+                file.RemoveAt(num - 1);
+                File.WriteAllLines(txtpath, file.ToArray());
+                Console.WriteLine($"({rmtodo}) has been removed!");
+            }
+        }
+
+        public static void ShowTodoCommand()
+        {
+            int count = 0;
+            foreach (string line in File.ReadLines(txtpath))
+            {
+                count++;
+                Console.WriteLine($"todo {count}: {line}");
+            }
+            Console.WriteLine($"total todos : {count}");
+        }
+        public static void ClearTodoCommand()
+        {
+            File.WriteAllText(txtpath, String.Empty);
+            Console.WriteLine("TodoList Clear!");
+        }
+    }
+}
---
+++ Todo/Todo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace todo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\CS_TodoList");
+            string txtpath = path + @"\todo.txt";
+
+            DirectoryInfo txtdir = new DirectoryInfo(txtpath);
+
+            if (args.Length < 1)
+            {
+                if (!File.Exists(txtpath))
+                {
+                    File.Create(txtpath);
+                    Console.WriteLine($"created txt file at \"{txtdir}\"!");
+                }
+                Function.WelCome();
+                return;
+            }
+
+
+            string option = args[0];
+            string str = "";
+            int result = 0;
+            switch (option.ToLower())
+            {
+                case "add":
+                    if(args.Length == 1)
+                    {
+                        Console.WriteLine("Command does not exist.");
+                    }
+                    for(int i=2; i<=args.Length; i++)
+                    {
+                        str += args[i-1] + " ";
+                    }
+                    Function.AddCommand(str);
+                    break;
+                case "rm":
+                    if (args.Length == 1 || args.Length >= 3)
+                    {
+                        Console.WriteLine("Command does not exist.");
+                        return;
+                    }
+                    if (int.TryParse(args[1], out result) != true)
+                    {
+                        Console.WriteLine("Command does not exist.");
+                        return;
+                    }
+                    Function.RemoveCommand(int.Parse(args[1]));
+                    break;
+                case "ls":
+                    Function.ShowTodoCommand();
+                    break;
+                case "clear":
+                    Function.ClearTodoCommand();
+                    break;
+                default:
+                    Console.WriteLine($"Command does not exist.");
+                    break;
+            }
+
+        }
+    }
+}
